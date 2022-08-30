03ff0e8593ec3689d31193bc6d7d13445cd0721a
sricharan173 <112267488+sricharan173@users.noreply.github.com>
2022-08-30 13:34:19 +0530

    Add files via upload

---
+++ 11.py
@@ -0,0 +1,8 @@
+string=input("enter the string:")
+character=len(string)
+word=string.split()
+word=string.count(string)
+print("number of character in string:")
+print(character)
+print("number of words in string:")
+print(word);
---
+++ reverse string.py
@@ -0,0 +1,5 @@
+string=(input("enter the string :"))
+if (string==string[::-1]):
+     print("reversed string",string)
+else:
+    print(string[::-1])
